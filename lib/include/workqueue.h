fc5bf22b38a18df38e0d21d56db04f7b78cb4291
Tom Haynes <loghyr@gmail.com>
2023-10-30 23:36:23 -0700

    Start of a workqueue
    Signed-off-by: Tom Haynes <loghyr@gmail.com>

---
+++ lib/include/workqueue.h
@@ -0,0 +1,11 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0+
+ * Copyright (C) 2023 Tom Haynes <loghyr@gmail.com>
+ */
+
+#ifndef _REFFS_WORKQUEUE_H
+#define _REFFS_WORKQUEUE_H
+
+struct workqueue;
+
+#endif /* _REFFS_WORKQUEUE_H */
