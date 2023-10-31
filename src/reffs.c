42ae4152831b64716cfa197f737e2ab166fe64d2
Tom Haynes <loghyr@gmail.com>
2023-10-30 23:31:35 -0700

    A stub main
    Signed-off-by: Tom Haynes <loghyr@gmail.com>

---
+++ src/reffs.c
@@ -0,0 +1,21 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0+
+ * Copyright (C) 2023 Tom Haynes <loghyr@gmail.com>
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <stdint.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <signal.h>
+
+int main(int argc, char **argv)
+{
+	printf("To be implemented\n");
+	
+	return 0;
+}
