edbde0ab9ca42a00c6208735062aff232dde6089
Tom Haynes <loghyr@gmail.com>
2023-11-04 00:03:10 -0700

    Flesh out lib framework and show how things will work
    Signed-off-by: Tom Haynes <loghyr@gmail.com>

---
+++ lib/include/reffs/log.h
@@ -0,0 +1,43 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Tom Haynes <loghyr@gmail.com>
+ * SPDX-License-Identifier: GPL-2.0+
+ */
+
+#ifndef _REFFS_LOG_H
+#define _REFFS_LOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdatomic.h>
+
+static inline void reffs_fail(const char *function, int line, const char *msg, ...)
+{
+	va_list ap;
+	va_start(ap, msg);
+	fprintf(stderr, "%s:%d ", function, line);
+	vfprintf(stderr, msg, ap);
+	fprintf(stderr, "\n", function, line);
+	va_end(ap);
+	abort();
+}
+
+static inline void reffs_log(const char *function, int line, const char *msg, ...)
+{
+	va_list ap;
+	va_start(ap, msg);
+	fprintf(stdout, "%s:%d ", function, line);
+	vfprintf(stdout, msg, ap);
+	fprintf(stdout, "\n", function, line);
+	va_end(ap);
+}
+
+#define FAIL(...) reffs_fail(__func__, __LINE__, __VA_ARGS__)
+
+#define WARN_ONCE(X, ...)                                     \
+	do {                                                  \
+		if (!atomic_flag_test_and_set((X)))           \
+			reffs_log(__func__, __LINE__, __VA_ARGS__); \
+	} while (0)
+
+#endif /* _REFFS_LOG_H */
--- src/echo.c
+++ src/echo.c
@@ -11,7 +11,6 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
 #include <string.h>
 #include <netdb.h>
 #include <unistd.h>
@@ -19,20 +18,9 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include "reffs/log.h"
 
 #define BUFSIZE 1024
-
-static inline void fail(const char *function, int line, const char *msg, ...)
-{
-	va_list ap;
-	va_start(ap, msg);
-	fprintf(stderr, "%s:%d ", function, line);
-	vfprintf(stderr, msg, ap);
-	va_end(ap);
-	abort();
-}
-
-#define FAIL(...) fail(__func__, __LINE__, __VA_ARGS__)
 
 int main(int argc, char *argv[])
 {
@@ -54,14 +42,22 @@ int main(int argc, char *argv[])
 	int len;
 	ssize_t n;
 
+	atomic_flag af;
+
+	atomic_flag_clear(&af); // Figure how to init it!
+
+	ret = 1;
+	WARN_ONCE(&af, "Running %s at %d", argv[0], ret++);
+	WARN_ONCE(&af, "Running %s at %d", argv[0], ret++);
+
 	if (argc != 2)
-		FAIL("usage: %s <port>\n", argv[0]);
+		FAIL("usage: %s <port>", argv[0]);
 
 	port = atoi(argv[1]);
 
 	listener = socket(AF_INET, SOCK_STREAM, 0);
 	if (listener < 0)
-		FAIL("Could not open socket: %d\n", listener);
+		FAIL("Could not open socket: %d", listener);
 
 	setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, (const void *)&opt,
 		   sizeof(int));
@@ -73,17 +69,17 @@ int main(int argc, char *argv[])
 
 	ret = bind(listener, (struct sockaddr *)&server, sizeof(server));
 	if (ret < 0)
-		FAIL("Could not bind server socket: %d\n", ret);
+		FAIL("Could not bind server socket: %d", ret);
 
 	ret = listen(listener, 5);
 	if (ret < 0)
-		FAIL("Could not listen on server socket: %d\n", ret);
+		FAIL("Could not listen on server socket: %d", ret);
 
 	len = sizeof(client);
 	while (1) {
 		connector = accept(listener, (struct sockaddr *)&client, &len);
 		if (connector < 0)
-			FAIL("Could not open socket: %d\n", connector);
+			FAIL("Could not open socket: %d", connector);
 
 		host = gethostbyaddr((const char *)&client.sin_addr.s_addr,
 				     sizeof(client.sin_addr.s_addr), AF_INET);
@@ -98,17 +94,22 @@ int main(int argc, char *argv[])
 			FAIL("Could not inet_ntoa(): %d", ret);
 		}
 
+		while (1) {
 		bzero(buf, BUFSIZE);
 		n = read(connector, buf, BUFSIZE);
 		if (n < 0)
-			FAIL("Could not read from socket: %d\n", n);
+			FAIL("Could not read from socket: %d", n);
 
-		printf("%s (aka %s) said %d bytes: %s\n", host->h_name,
+		printf("%s (aka %s) said %d bytes: %s", host->h_name,
 		       hostaddr, n, buf);
 
 		n = write(connector, buf, strlen(buf));
 		if (n < 0)
-			FAIL("Could not write to socket: %d\n", n);
+			FAIL("Could not write to socket: %d", n);
+
+		if (!strncmp(buf, "kill", 4) || !strncmp(buf, "done", 4))
+			break;
+		}
 
 		close(connector);
 
