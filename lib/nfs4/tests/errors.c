b269e371cb739b793dd46c06a1f4580f8c6adaee
Tom Haynes <loghyr@gmail.com>
2023-11-12 02:45:26 -0800

    Ditch check, but still not working from top directory
    Signed-off-by: Tom Haynes <loghyr@gmail.com>

--- lib/nfs4/tests/errors.c
+++ lib/nfs4/tests/errors.c
@@ -3,42 +3,16 @@
  * SPDX-License-Identifier: GPL-2.0+
  */
 
-#include <check.h>
-#include <stdbool.h>
 #include "errors.h"
 #include "reffs/test.h"
 
-START_TEST (error1)
-{
-	ck_assert_int_eq(nfs4_error_valid_for_op(OP_ACCESS, NFS4ERR_ACCESS), true);
-}
-
-START_TEST (error2)
-{
-	ck_assert_int_eq(nfs4_error_valid_for_op(OP_ACCESS, NFS4ERR_ADMIN_REVOKED), false);
-}
-
-START_TEST (error3)
-{
-	ck_assert_int_eq(nfs4_error_valid_for_op(OP_WRITE_SAME, NFS4ERR_WRONG_TYPE), true);
-}
-
-START_TEST (error4)
-{
-	ck_assert_int_eq(nfs4_error_valid_for_op(OP_WRITE_SAME, NFS4ERR_SEQ_FALSE_RETRY), false);
-}
-
-START_TEST (error5)
-{
-	ck_assert_int_eq(nfs4_error_valid_for_cb_op(OP_CB_WANTS_CANCELLED, NFS4ERR_DELAY), true);
-}
-
-START_TEST (error6)
-{
-	ck_assert_int_eq(nfs4_error_valid_for_cb_op(OP_CB_WANTS_CANCELLED, NFS4ERR_SEQ_MISORDERED), false);
-}
-
 int main(void)
 {
+	verify(nfs4_error_valid_for_op(OP_ACCESS, NFS4ERR_ACCESS));
+	verify(!nfs4_error_valid_for_op(OP_ACCESS, NFS4ERR_ADMIN_REVOKED));
+	verify(nfs4_error_valid_for_op(OP_WRITE_SAME, NFS4ERR_WRONG_TYPE));
+	verify(!nfs4_error_valid_for_op(OP_WRITE_SAME, NFS4ERR_SEQ_FALSE_RETRY));
+	verify(nfs4_error_valid_for_cb_op(OP_CB_WANTS_CANCELLED, NFS4ERR_DELAY));
+	verify(!nfs4_error_valid_for_cb_op(OP_CB_WANTS_CANCELLED, NFS4ERR_SEQ_MISORDERED));
 	return 0;
 }
