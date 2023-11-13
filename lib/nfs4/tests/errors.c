98f756c0a9ebc54463096663d23f4a9b7b48a80a
Tom Haynes <loghyr@gmail.com>
2023-11-12 23:32:08 -0800

    Yeah, got libcheck working!
    I had failed to install check-devel...
    
    Signed-off-by: Tom Haynes <loghyr@gmail.com>

--- lib/nfs4/tests/errors.c
+++ lib/nfs4/tests/errors.c
@@ -3,19 +3,85 @@
  * SPDX-License-Identifier: GPL-2.0+
  */
 
+#include <stdlib.h>
+#include <check.h>
+#include <stdbool.h>
 #include "errors.h"
 #include "reffs/test.h"
 
+START_TEST(test_error1)
+{
+	ck_assert_int_eq(nfs4_error_valid_for_op(OP_ACCESS, NFS4ERR_ACCESS),
+			 true);
+}
+
+START_TEST(test_error2)
+{
+	ck_assert_int_eq(nfs4_error_valid_for_op(OP_ACCESS,
+						 NFS4ERR_ADMIN_REVOKED),
+			 false);
+}
+
+START_TEST(test_error3)
+{
+	ck_assert_int_eq(nfs4_error_valid_for_op(OP_WRITE_SAME,
+						 NFS4ERR_WRONG_TYPE),
+			 true);
+}
+
+START_TEST(test_error4)
+{
+	ck_assert_int_eq(nfs4_error_valid_for_op(OP_WRITE_SAME,
+						 NFS4ERR_SEQ_FALSE_RETRY),
+			 false);
+}
+
+START_TEST(test_error5)
+{
+	ck_assert_int_eq(nfs4_error_valid_for_cb_op(OP_CB_WANTS_CANCELLED,
+						    NFS4ERR_DELAY),
+			 true);
+}
+
+START_TEST(test_error6)
+{
+	ck_assert_int_eq(nfs4_error_valid_for_cb_op(OP_CB_WANTS_CANCELLED,
+						    NFS4ERR_SEQ_MISORDERED),
+			 false);
+}
+
+Suite *error_suite(void)
+{
+	Suite *s;
+	TCase *tc_core;
+
+	s = suite_create("NFSv4 Errors");
+
+	/* Core test case */
+	tc_core = tcase_create("Core");
+
+	tcase_add_test(tc_core, test_error1);
+	tcase_add_test(tc_core, test_error2);
+	tcase_add_test(tc_core, test_error3);
+	tcase_add_test(tc_core, test_error4);
+	tcase_add_test(tc_core, test_error5);
+	tcase_add_test(tc_core, test_error6);
+	suite_add_tcase(s, tc_core);
+
+	return s;
+}
+
 int main(void)
 {
-	verify(nfs4_error_valid_for_op(OP_ACCESS, NFS4ERR_ACCESS));
-	verify(!nfs4_error_valid_for_op(OP_ACCESS, NFS4ERR_ADMIN_REVOKED));
-	verify(nfs4_error_valid_for_op(OP_WRITE_SAME, NFS4ERR_WRONG_TYPE));
-	verify(!nfs4_error_valid_for_op(OP_WRITE_SAME,
-					NFS4ERR_SEQ_FALSE_RETRY));
-	verify(nfs4_error_valid_for_cb_op(OP_CB_WANTS_CANCELLED,
-					  NFS4ERR_DELAY));
-	verify(!nfs4_error_valid_for_cb_op(OP_CB_WANTS_CANCELLED,
-					   NFS4ERR_SEQ_MISORDERED));
-	return 0;
+	int number_failed;
+	Suite *s;
+	SRunner *sr;
+
+	s = error_suite();
+	sr = srunner_create(s);
+
+	srunner_run_all(sr, CK_NORMAL);
+	number_failed = srunner_ntests_failed(sr);
+	srunner_free(sr);
+	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
