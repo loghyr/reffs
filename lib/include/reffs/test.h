bf9b3135300fc36d83efe236e7993b0cf9944778
Tom Haynes <loghyr@gmail.com>
2025-03-14 20:28:17 -0700

    Refactor to share code
    Signed-off-by: Tom Haynes <loghyr@gmail.com>

--- lib/include/reffs/test.h
+++ lib/include/reffs/test.h
@@ -15,35 +15,31 @@
 #define assert_ptr(expr, ...) (__ASSERT_VOID_CAST(0))
 #define assert_status(expr, ...) (__ASSERT_VOID_CAST(0))
 #else
-#define assert_ptr(expr, ...)                                                  \
-	do {                                                                   \
-		if ((expr) == 0) {                                             \
-			fprintf(stderr, "%s:%d assert %s", __func__, __LINE__, \
-				#expr);                                        \
-			fprintf(stderr, __VA_ARGS__);                          \
-			fprintf(stderr, "\n");                                 \
-			abort();                                               \
-		}                                                              \
+#define assert_ptr(expr, ...)                                        \
+	do {                                                         \
+		if ((expr) == 0) {                                   \
+			reffs_fail(__func__, __LINE__,               \
+				   "assert %s: " __VA_ARGS__, #expr, \
+				   ##__VA_ARGS__);                   \
+		}                                                    \
 	} while (0)
-#define assert_status(expr, ...)                                           \
-	do {                                                               \
-		if ((expr) != 0) {                                         \
-			fprintf(stderr, "%s:%d assert status %s is not 0", \
-				__func__, __LINE__, #expr);                \
-			fprintf(stderr, __VA_ARGS__);                      \
-			fprintf(stderr, "\n");                             \
-			abort();                                           \
-		}                                                          \
+
+#define assert_status(expr, ...)                                              \
+	do {                                                                  \
+		if ((expr) != 0) {                                            \
+			reffs_fail(__func__, __LINE__,                        \
+				   "assert status %s is not 0: " __VA_ARGS__, \
+				   #expr, ##__VA_ARGS__);                     \
+		}                                                             \
 	} while (0)
+
 #endif /* NDEBUG */
 
-#define verify(expr)                                                   \
-	do {                                                           \
-		if ((expr) == 0) {                                     \
-			fprintf(stderr, "%s:%d assert %s\n", __func__, \
-				__LINE__, #expr);                      \
-			abort();                                       \
-		}                                                      \
+#define verify(expr)                                                        \
+	do {                                                                \
+		if ((expr) == 0) {                                          \
+			reffs_fail(__func__, __LINE__, "assert %s", #expr); \
+		}                                                           \
 	} while (0)
 
 #endif /* _REFFS_TEST_H */
