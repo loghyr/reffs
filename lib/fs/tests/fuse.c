1d169848c3c17355df881b970773e372f95f37fc
Tom Haynes <loghyr@gmail.com>
2025-04-03 08:45:36 -0700

    Beef up the fuse test cases with rename examples
    Signed-off-by: Tom Haynes <loghyr@gmail.com>

--- lib/fs/tests/fuse.c
+++ lib/fs/tests/fuse.c
@@ -37,6 +37,8 @@ int main(void)
 	struct inode *inode;
 
 	struct stat st_pre;
+	struct stat st_src_pre;
+	struct stat st_dst_pre;
 	struct stat st_post;
 
 	char buffer[BUFFER_LEN];
@@ -68,18 +70,54 @@ int main(void)
 	inode->i_used = 8;
 	inode->i_nlink = 2;
 
+	ret = reffs_fuse_getattr("/", &st_pre);
+	verify(!ret);
+	verify(st_pre.st_nlink == 2);
+
 	ret = reffs_fuse_mkdir("/foo", 0755);
 	verify(!ret);
+	ret = reffs_fuse_getattr("/foo", &st_post);
+	verify(!ret);
+	verify(st_post.st_nlink == 2);
+
+	ret = reffs_fuse_getattr("/", &st_post);
+	verify(!ret);
+	verify(st_pre.st_nlink == st_post.st_nlink - 1);
+	st_pre = st_post;
+
 	ret = reffs_fuse_mkdir("/foo/bar", 0640);
 	verify(!ret);
+	ret = reffs_fuse_getattr("/foo", &st_pre);
+	verify(!ret);
+	verify(st_pre.st_nlink == 3);
+
 	ret = reffs_fuse_mkdir("/foo/garbo", 0640);
 	verify(!ret);
+	ret = reffs_fuse_getattr("/foo", &st_pre);
+	verify(!ret);
+	verify(st_pre.st_nlink == 4);
+
 	ret = reffs_fuse_mkdir("/foo/nurse", 0640);
 	verify(!ret);
+	ret = reffs_fuse_getattr("/foo", &st_pre);
+	verify(!ret);
+	verify(st_pre.st_nlink == 5);
+
 	ret = reffs_fuse_mkdir("/hello", 0755);
 	verify(!ret);
+	ret = reffs_fuse_getattr("/hello", &st_pre);
+	verify(!ret);
+	verify(st_pre.st_nlink == 2);
+
+	ret = reffs_fuse_getattr("/", &st_pre);
+	verify(!ret);
+	verify(st_pre.st_nlink == 4);
+
 	ret = reffs_fuse_mkdir("/hello/nurse", 0755);
 	verify(!ret);
+	ret = reffs_fuse_getattr("/hello", &st_pre);
+	verify(!ret);
+	verify(st_pre.st_nlink == 3);
 
 	ret = reffs_fuse_getattr("/foo/bar", &st_pre);
 	verify(!ret);
@@ -311,6 +349,190 @@ int main(void)
 	verify(!ret);
 	ret = reffs_fuse_rmdir("/hello");
 	verify(!ret);
+
+	ret = reffs_fuse_getattr("/", &st_pre);
+	verify(!ret);
+	verify(st_pre.st_nlink == 2);
+
+	ret = reffs_fuse_mkdir("/foo", 0755);
+	verify(!ret);
+
+	ret = reffs_fuse_getattr("/", &st_post);
+	verify(!ret);
+	verify(st_pre.st_nlink == st_post.st_nlink - 1);
+
+	ret = reffs_fuse_getattr("/foo", &st_pre);
+	verify(!ret);
+
+	ret = reffs_fuse_create("/nurse", S_IFREG | 0755, NULL);
+	verify(!ret);
+
+	ret = reffs_fuse_getattr("/nurse", &st_pre);
+	verify(!ret);
+	verify(st_pre.st_uid == inode->i_uid);
+	verify(st_pre.st_gid == inode->i_gid);
+	verify(st_pre.st_size == 0);
+	verify(st_pre.st_mode == (S_IFREG | 0755));
+
+	usleep(1000);
+
+	ret = reffs_fuse_write("/nurse", "hello", 5, 0, NULL);
+	verify(ret == 5);
+
+	ret = reffs_fuse_getattr("/nurse", &st_post);
+	verify(!ret);
+	verify(st_pre.st_ino == st_post.st_ino);
+	verify(st_pre.st_uid == st_post.st_uid);
+	verify(st_pre.st_gid == st_post.st_gid);
+	verify(st_post.st_size == 5);
+	verify(st_pre.st_atim.tv_sec == st_post.st_atim.tv_sec);
+	verify(st_pre.st_atim.tv_nsec == st_post.st_atim.tv_nsec);
+	verify((st_pre.st_mtim.tv_sec < st_post.st_mtim.tv_sec) ||
+	       ((st_pre.st_mtim.tv_sec == st_post.st_mtim.tv_sec) &&
+		((st_pre.st_mtim.tv_nsec < st_post.st_mtim.tv_nsec))));
+	verify((st_pre.st_ctim.tv_sec < st_post.st_ctim.tv_sec) ||
+	       ((st_pre.st_ctim.tv_sec == st_post.st_ctim.tv_sec) &&
+		((st_pre.st_ctim.tv_nsec < st_post.st_ctim.tv_nsec))));
+
+	st_pre = st_post;
+
+	ret = reffs_fuse_getattr("/", &st_src_pre);
+	verify(!ret);
+
+	ret = reffs_fuse_getattr("/foo", &st_dst_pre);
+	verify(!ret);
+
+	usleep(1000);
+
+	ret = reffs_fuse_rename("/nurse", "/foo/nurse");
+	verify(!ret);
+
+	ret = reffs_fuse_getattr("/nurse", &st_post);
+	verify(ret == -ENOENT);
+
+	ret = reffs_fuse_getattr("/foo/nurse", &st_post);
+	verify(!ret);
+	verify(st_pre.st_ino == st_post.st_ino);
+	verify(st_pre.st_uid == st_post.st_uid);
+	verify(st_pre.st_gid == st_post.st_gid);
+	verify(st_post.st_size == 5);
+	verify(st_pre.st_atim.tv_sec == st_post.st_atim.tv_sec);
+	verify(st_pre.st_atim.tv_nsec == st_post.st_atim.tv_nsec);
+	verify((st_pre.st_mtim.tv_sec == st_post.st_mtim.tv_sec) &&
+	       (st_pre.st_mtim.tv_nsec == st_post.st_mtim.tv_nsec)); 
+	verify((st_pre.st_ctim.tv_sec == st_post.st_ctim.tv_sec) &&
+	       (st_pre.st_ctim.tv_nsec == st_post.st_ctim.tv_nsec)); 
+	st_pre = st_post;
+
+	ret = reffs_fuse_getattr("/", &st_post);
+	verify(!ret);
+	verify(st_src_pre.st_ino == st_post.st_ino);
+	verify(st_src_pre.st_uid == st_post.st_uid);
+	verify(st_src_pre.st_gid == st_post.st_gid);
+	verify(st_src_pre.st_nlink == st_post.st_nlink + 1);
+	verify(st_src_pre.st_atim.tv_sec == st_post.st_atim.tv_sec);
+	verify(st_src_pre.st_atim.tv_nsec == st_post.st_atim.tv_nsec);
+	verify((st_src_pre.st_mtim.tv_sec < st_post.st_mtim.tv_sec) ||
+	       ((st_src_pre.st_mtim.tv_sec == st_post.st_mtim.tv_sec) &&
+		((st_src_pre.st_mtim.tv_nsec < st_post.st_mtim.tv_nsec))));
+	verify((st_src_pre.st_ctim.tv_sec < st_post.st_ctim.tv_sec) ||
+	       ((st_src_pre.st_ctim.tv_sec == st_post.st_ctim.tv_sec) &&
+		((st_src_pre.st_ctim.tv_nsec < st_post.st_ctim.tv_nsec))));
+	st_src_pre = st_post;
+
+	ret = reffs_fuse_getattr("/foo", &st_post);
+	verify(!ret);
+	verify(st_dst_pre.st_ino == st_post.st_ino);
+	verify(st_dst_pre.st_uid == st_post.st_uid);
+	verify(st_dst_pre.st_gid == st_post.st_gid);
+	verify(st_dst_pre.st_nlink == st_post.st_nlink - 1);
+	verify(st_dst_pre.st_atim.tv_sec == st_post.st_atim.tv_sec);
+	verify(st_dst_pre.st_atim.tv_nsec == st_post.st_atim.tv_nsec);
+	verify((st_dst_pre.st_mtim.tv_sec < st_post.st_mtim.tv_sec) ||
+	       ((st_dst_pre.st_mtim.tv_sec == st_post.st_mtim.tv_sec) &&
+		((st_dst_pre.st_mtim.tv_nsec < st_post.st_mtim.tv_nsec))));
+	verify((st_dst_pre.st_ctim.tv_sec < st_post.st_ctim.tv_sec) ||
+	       ((st_dst_pre.st_ctim.tv_sec == st_post.st_ctim.tv_sec) &&
+		((st_dst_pre.st_ctim.tv_nsec < st_post.st_ctim.tv_nsec))));
+	st_dst_pre = st_post;
+
+	usleep(1000);
+
+	ret = reffs_fuse_rename("/foo/nurse", "/nurse");
+	verify(!ret);
+
+	ret = reffs_fuse_getattr("/foo/nurse", &st_post);
+	verify(ret == -ENOENT);
+
+	ret = reffs_fuse_getattr("/nurse", &st_post);
+	verify(!ret);
+	verify(st_pre.st_ino == st_post.st_ino);
+	verify(st_pre.st_uid == st_post.st_uid);
+	verify(st_pre.st_gid == st_post.st_gid);
+	verify(st_post.st_size == 5);
+	verify(st_pre.st_atim.tv_sec == st_post.st_atim.tv_sec);
+	verify(st_pre.st_atim.tv_nsec == st_post.st_atim.tv_nsec);
+	verify((st_pre.st_mtim.tv_sec == st_post.st_mtim.tv_sec) &&
+	       (st_pre.st_mtim.tv_nsec == st_post.st_mtim.tv_nsec)); 
+	verify((st_pre.st_ctim.tv_sec == st_post.st_ctim.tv_sec) &&
+	       (st_pre.st_ctim.tv_nsec == st_post.st_ctim.tv_nsec)); 
+	st_pre = st_post;
+
+	ret = reffs_fuse_getattr("/", &st_post);
+	verify(!ret);
+	verify(st_src_pre.st_ino == st_post.st_ino);
+	verify(st_src_pre.st_uid == st_post.st_uid);
+	verify(st_src_pre.st_gid == st_post.st_gid);
+	verify(st_src_pre.st_nlink == st_post.st_nlink - 1);
+	verify(st_src_pre.st_atim.tv_sec == st_post.st_atim.tv_sec);
+	verify(st_src_pre.st_atim.tv_nsec == st_post.st_atim.tv_nsec);
+	verify((st_src_pre.st_mtim.tv_sec < st_post.st_mtim.tv_sec) ||
+	       ((st_src_pre.st_mtim.tv_sec == st_post.st_mtim.tv_sec) &&
+		((st_src_pre.st_mtim.tv_nsec < st_post.st_mtim.tv_nsec))));
+	verify((st_src_pre.st_ctim.tv_sec < st_post.st_ctim.tv_sec) ||
+	       ((st_src_pre.st_ctim.tv_sec == st_post.st_ctim.tv_sec) &&
+		((st_src_pre.st_ctim.tv_nsec < st_post.st_ctim.tv_nsec))));
+	st_src_pre = st_post;
+
+	ret = reffs_fuse_getattr("/foo", &st_post);
+	verify(!ret);
+	verify(st_dst_pre.st_ino == st_post.st_ino);
+	verify(st_dst_pre.st_uid == st_post.st_uid);
+	verify(st_dst_pre.st_gid == st_post.st_gid);
+	verify(st_dst_pre.st_nlink == st_post.st_nlink + 1);
+	verify(st_dst_pre.st_atim.tv_sec == st_post.st_atim.tv_sec);
+	verify(st_dst_pre.st_atim.tv_nsec == st_post.st_atim.tv_nsec);
+	verify((st_dst_pre.st_mtim.tv_sec < st_post.st_mtim.tv_sec) ||
+	       ((st_dst_pre.st_mtim.tv_sec == st_post.st_mtim.tv_sec) &&
+		((st_dst_pre.st_mtim.tv_nsec < st_post.st_mtim.tv_nsec))));
+	verify((st_dst_pre.st_ctim.tv_sec < st_post.st_ctim.tv_sec) ||
+	       ((st_dst_pre.st_ctim.tv_sec == st_post.st_ctim.tv_sec) &&
+		((st_dst_pre.st_ctim.tv_nsec < st_post.st_ctim.tv_nsec))));
+	st_dst_pre = st_post;
+
+	usleep(1000);
+
+	ret = reffs_fuse_unlink("/nurse");
+	verify(!ret);
+
+	ret = reffs_fuse_rmdir("/foo");
+	verify(!ret);
+
+	ret = reffs_fuse_getattr("/", &st_post);
+	verify(!ret);
+	verify(st_src_pre.st_ino == st_post.st_ino);
+	verify(st_src_pre.st_uid == st_post.st_uid);
+	verify(st_src_pre.st_gid == st_post.st_gid);
+	verify(st_post.st_nlink == 2);
+	verify(st_src_pre.st_nlink == st_post.st_nlink + 2);
+	verify(st_src_pre.st_atim.tv_sec == st_post.st_atim.tv_sec);
+	verify(st_src_pre.st_atim.tv_nsec == st_post.st_atim.tv_nsec);
+	verify((st_src_pre.st_mtim.tv_sec < st_post.st_mtim.tv_sec) ||
+	       ((st_src_pre.st_mtim.tv_sec == st_post.st_mtim.tv_sec) &&
+		((st_src_pre.st_mtim.tv_nsec < st_post.st_mtim.tv_nsec))));
+	verify((st_src_pre.st_ctim.tv_sec < st_post.st_ctim.tv_sec) ||
+	       ((st_src_pre.st_ctim.tv_sec == st_post.st_ctim.tv_sec) &&
+		((st_src_pre.st_ctim.tv_nsec < st_post.st_ctim.tv_nsec))));
 
 	super_block_dirent_release(root_sb, reffs_life_action_death);
 	inode_put(inode);
