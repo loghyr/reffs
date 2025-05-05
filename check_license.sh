770cbe13f4e6d6355734a6bec8a662fc0566fa5a
Tom Haynes <loghyr@gmail.com>
2025-05-05 13:16:54 -0700

    Add a python client for probing reffs servers
    Signed-off-by: Tom Haynes <loghyr@gmail.com>

--- check_license.sh
+++ check_license.sh
@@ -27,8 +27,8 @@ check_spdx_headers() {
   fi
 }
 
-# Get a list of all files, excluding .gitignore, LICENSES directory, .x files, COPYING, and xdrgen.py.in
-files=$(git ls-files | grep -vE '\.gitignore$|LICENSES/|\.x$|COPYING$|lib/scripts/reffs/xdrgen\.py\.in$')
+# Get a list of all files, excluding .gitignore, LICENSES directory, .x files, COPYING, and the entire lib/scripts/reffs/pynfs directory
+files=$(git ls-files | grep -vE '\.gitignore$|LICENSES/|\.x$|COPYING$|lib/scripts/reffs/pynfs/')
 
 # Check each file
 error_count=0
---
+++ scripts/reffs/pynfs/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+from __future__ import print_function
+
+from setuptools import setup
+
+import sys
+import os
+from os.path import join
+
+DESCRIPTION = """
+pynfs
+============
+
+Add stuff here.
+"""
+
+DIRS = ["xdr", "rpc", "nfs4.1", "nfs4.0"] # Order is important
+
+def setup(*args, **kwargs):
+    cwd = os.getcwd()
+    command = " ".join(sys.argv)
+    for dir in DIRS:
+        print("\n\nMoving to %s" % dir )
+        os.chdir(join(cwd, dir))
+        os.system("python%s %s" % (sys.version[0], command))
+    os.chdir(cwd)
+
+setup(name = "pynfs",
+      version = "0.0.0", # import this?
+      packages = ["nfs4", "rpc", "xdr"],
+      description = "NFS tools, tests, and support libraries",
+      long_description = DESCRIPTION,
+      #install_requires = ["gssapi", "ply"],
+
+      # These will be the same
+      author = "Fred Isaman",
+      author_email = "iisaman@citi.umich.edu",
+      maintainer = "Calum Mackay",
+      maintainer_email = "calum.mackay@oracle.com",
+      url = "https://linux-nfs.org/wiki/index.php/Pynfs",
+      license = "GPL"
+      
+      )
+
