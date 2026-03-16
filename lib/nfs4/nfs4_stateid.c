1ecd380d88c72110036184ac5e72672110ed7c0f
Tom Haynes <loghyr@gmail.com>
2026-03-16 09:58:28 -0700

    fs: Split stateid into fs and nfs4 parts
    Signed-off-by: Tom Haynes <loghyr@gmail.com>

---
+++ lib/nfs4/nfs4_stateid.c
@@ -0,0 +1,196 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Tom Haynes <loghyr@gmail.com>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h" // IWYU pragma: keep
+#endif
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#include <urcu/compiler.h>
+#include <urcu/rculfhash.h>
+
+#include "nfsv42_xdr.h"
+#include "reffs/log.h"
+#include "reffs/inode.h"
+#include "reffs/stateid.h"
+#include "nfs4_stateid.h"
+
+/* ------------------------------------------------------------------ */
+/* Wire pack / unpack                                                  */
+
+void pack_stateid4(stateid4 *st, struct stateid *stid)
+{
+	uint8_t *other = (uint8_t *)st->other;
+	uint32_t tag = stid->s_tag;
+
+	st->seqid = __atomic_load_n(&stid->s_seqid, __ATOMIC_RELAXED);
+
+	memcpy(other + 0, &stid->s_id, sizeof(uint32_t));
+	memcpy(other + 4, &tag, sizeof(uint32_t));
+	memcpy(other + 8, &stid->s_cookie, sizeof(uint32_t));
+}
+
+void unpack_stateid4(const stateid4 *st, uint32_t *seqid, uint32_t *id,
+		     uint32_t *type, uint32_t *cookie)
+{
+	const uint8_t *other = (const uint8_t *)st->other;
+
+	*seqid = st->seqid;
+	memcpy(id, other + 0, sizeof(uint32_t));
+	memcpy(type, other + 4, sizeof(uint32_t));
+	memcpy(cookie, other + 8, sizeof(uint32_t));
+}
+
+/* ------------------------------------------------------------------ */
+/* Open stateid                                                        */
+
+static void open_stateid_free_rcu(struct rcu_head *rcu)
+{
+	struct stateid *stid = caa_container_of(rcu, struct stateid, s_rcu);
+	struct open_stateid *os = stid_to_open(stid);
+
+	free(os);
+}
+
+static void open_stateid_release(struct stateid *stid)
+{
+	call_rcu(&stid->s_rcu, open_stateid_free_rcu);
+}
+
+struct open_stateid *open_stateid_alloc(struct inode *inode)
+{
+	struct open_stateid *os;
+	int ret;
+
+	os = calloc(1, sizeof(*os));
+	if (!os) {
+		LOG("open_stateid_alloc: OOM");
+		return NULL;
+	}
+
+	ret = stateid_assign(&os->os_stid, inode, Open_Stateid,
+			     open_stateid_free_rcu, open_stateid_release);
+	if (ret) {
+		free(os);
+		return NULL;
+	}
+
+	return os;
+}
+
+/* ------------------------------------------------------------------ */
+/* Delegation stateid                                                  */
+
+static void delegation_stateid_free_rcu(struct rcu_head *rcu)
+{
+	struct stateid *stid = caa_container_of(rcu, struct stateid, s_rcu);
+	struct delegation_stateid *ds = stid_to_delegation(stid);
+
+	free(ds);
+}
+
+static void delegation_stateid_release(struct stateid *stid)
+{
+	call_rcu(&stid->s_rcu, delegation_stateid_free_rcu);
+}
+
+struct delegation_stateid *delegation_stateid_alloc(struct inode *inode)
+{
+	struct delegation_stateid *ds;
+	int ret;
+
+	ds = calloc(1, sizeof(*ds));
+	if (!ds) {
+		LOG("delegation_stateid_alloc: OOM");
+		return NULL;
+	}
+
+	ret = stateid_assign(&ds->ds_stid, inode, Delegation_Stateid,
+			     delegation_stateid_free_rcu,
+			     delegation_stateid_release);
+	if (ret) {
+		free(ds);
+		return NULL;
+	}
+
+	return ds;
+}
+
+/* ------------------------------------------------------------------ */
+/* Lock stateid                                                        */
+
+static void lock_stateid_free_rcu(struct rcu_head *rcu)
+{
+	struct stateid *stid = caa_container_of(rcu, struct stateid, s_rcu);
+	struct lock_stateid *ls = stid_to_lock(stid);
+
+	free(ls);
+}
+
+static void lock_stateid_release(struct stateid *stid)
+{
+	call_rcu(&stid->s_rcu, lock_stateid_free_rcu);
+}
+
+struct lock_stateid *lock_stateid_alloc(struct inode *inode)
+{
+	struct lock_stateid *ls;
+	int ret;
+
+	ls = calloc(1, sizeof(*ls));
+	if (!ls) {
+		LOG("lock_stateid_alloc: OOM");
+		return NULL;
+	}
+
+	ret = stateid_assign(&ls->ls_stid, inode, Lock_Stateid,
+			     lock_stateid_free_rcu, lock_stateid_release);
+	if (ret) {
+		free(ls);
+		return NULL;
+	}
+
+	return ls;
+}
+
+/* ------------------------------------------------------------------ */
+/* Layout stateid                                                      */
+
+static void layout_stateid_free_rcu(struct rcu_head *rcu)
+{
+	struct stateid *stid = caa_container_of(rcu, struct stateid, s_rcu);
+	struct layout_stateid *ls = stid_to_layout(stid);
+
+	free(ls);
+}
+
+static void layout_stateid_release(struct stateid *stid)
+{
+	call_rcu(&stid->s_rcu, layout_stateid_free_rcu);
+}
+
+struct layout_stateid *layout_stateid_alloc(struct inode *inode)
+{
+	struct layout_stateid *ls;
+	int ret;
+
+	ls = calloc(1, sizeof(*ls));
+	if (!ls) {
+		LOG("layout_stateid_alloc: OOM");
+		return NULL;
+	}
+
+	ret = stateid_assign(&ls->ls_stid, inode, Layout_Stateid,
+			     layout_stateid_free_rcu, layout_stateid_release);
+	if (ret) {
+		free(ls);
+		return NULL;
+	}
+
+	return ls;
+}
