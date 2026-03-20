087fbd36d4910b3e459dd7b526d90b25aae25488
Tom Haynes <loghyr@gmail.com>
2026-03-20 11:04:45 -0700

    nfs4: add task state machine fields to struct task
    Add task_state enum (IDLE/RUNNING/PAUSED/DONE) and two new fields to
    struct task:
    
      _Atomic enum task_state t_state  -- atomic state for ownership transfer
      struct rpc_trans *t_rt           -- back-pointer set once rpc_trans allocated
    
    These are the foundational types for the pausable compound state machine.
    No behavior change; t_state initialises to TASK_IDLE (0) via calloc and
    t_rt initialises to NULL.
    
    Signed-off-by: Tom Haynes <loghyr@gmail.com>

--- lib/include/reffs/task.h
+++ lib/include/reffs/task.h
@@ -6,12 +6,20 @@
 #ifndef _REFFS_TASK_H
 #define _REFFS_TASK_H
 
+#include <stdatomic.h>
 #include <unistd.h>
 #include <liburing.h>
 #include "reffs/ring.h"
 #include "reffs/network.h"
 
 struct rpc_trans;
+
+enum task_state {
+	TASK_IDLE = 0,
+	TASK_RUNNING = 1,
+	TASK_PAUSED = 2,
+	TASK_DONE = 3,
+};
 
 // Task struct for worker queue
 struct task {
@@ -22,6 +30,8 @@ struct task {
 	struct connection_info t_ci;
 	struct ring_context *t_rc;
 	int (*t_cb)(struct rpc_trans *rt);
+	_Atomic enum task_state t_state;
+	struct rpc_trans *t_rt; /* non-NULL once rpc_trans is allocated */
 };
 
 #endif /* _REFFS_TASK_H */
