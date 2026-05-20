058ee308cf5bcec6a5c04e37823456a2e6c7014c
Tom Haynes <loghyr@gmail.com>
2026-05-20 11:06:39 -0700

    Revert "DIAG: trace cstats path entry in nfs4_op_chunk_write"
    This reverts commit 3b8d572a333ba9bcdd691ca60ba19a9ba5cff46f.

--- lib/nfs4/server/chunk.c
+++ lib/nfs4/server/chunk.c
@@ -222,11 +222,6 @@ uint32_t nfs4_op_chunk_write(struct compound *compound)
 		compound->c_curr_sb ? &compound->c_curr_sb->sb_chunk_stats :
 				      NULL;
 
-	/* INV-1 diag: confirm cstats path is reached on the DS. */
-	TRACE("INV1_DIAG: chunk_write nchunks=%u c_curr_sb=%p cstats=%p sb_id=%lu",
-	      nchunks, (void *)compound->c_curr_sb, (void *)cstats,
-	      compound->c_curr_sb ? compound->c_curr_sb->sb_id : 0UL);
-
 	/* Record per-block metadata.  Last block may be smaller. */
 	for (uint32_t i = 0; i < nchunks; i++) {
 		uint32_t blk_size = chunk_size;
