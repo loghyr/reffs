loghyr/reffs

commit 54d71614417a9f4d8042c6d3960a2b3b63860dd1

nfs4: implement Flex Files v2 checksums

third_party/blake3/blake3_dispatch.c
@@ -1,0 +1,334 @@
+/* SPDX-FileCopyrightText: 2019 BLAKE3 Authors */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include "blake3_impl.h"
+
+#if defined(_MSC_VER)
+#include <Windows.h>
+#endif
+
+#if defined(IS_X86)
+#if defined(_MSC_VER)
+#include <intrin.h>
+#elif defined(__GNUC__)
+#include <immintrin.h>
+#else
+#undef IS_X86 /* Unimplemented! */
+#endif
+#endif
+
+#if !defined(BLAKE3_ATOMICS)
+#if defined(__has_include)
+#if __has_include(<stdatomic.h>) && !defined(_MSC_VER)
+#define BLAKE3_ATOMICS 1
+#else
+#define BLAKE3_ATOMICS 0
+#endif /* __has_include(<stdatomic.h>) && !defined(_MSC_VER) */
+#else
+#define BLAKE3_ATOMICS 0
+#endif /* defined(__has_include) */
+#endif /* BLAKE3_ATOMICS */
+
+#if BLAKE3_ATOMICS
+#define ATOMIC_INT _Atomic int
+#define ATOMIC_LOAD(x) x
+#define ATOMIC_STORE(x, y) x = y
+#elif defined(_MSC_VER)
+#define ATOMIC_INT LONG
+#define ATOMIC_LOAD(x) InterlockedOr(&x, 0)
+#define ATOMIC_STORE(x, y) InterlockedExchange(&x, y)
+#else
+#define ATOMIC_INT int
+#define ATOMIC_LOAD(x) x
+#define ATOMIC_STORE(x, y) x = y
+#endif
+
+#define MAYBE_UNUSED(x) (void)((x))
+
+#if defined(IS_X86)
+static uint64_t xgetbv(void) {
+#if defined(_MSC_VER)
+  return _xgetbv(0);
+#else
+  uint32_t eax = 0, edx = 0;
+  __asm__ __volatile__("xgetbv\n" : "=a"(eax), "=d"(edx) : "c"(0));
+  return ((uint64_t)edx << 32) | eax;
+#endif
+}
+static void cpuid(uint32_t out[4], uint32_t id) {
+#if defined(_MSC_VER)
+  __cpuid((int *)out, id);
+#elif defined(__i386__) || defined(_M_IX86)
+  __asm__ __volatile__("movl %%ebx, %1\n"
+                       "cpuid\n"
+                       "xchgl %1, %%ebx\n"
+                       : "=a"(out[0]), "=r"(out[1]), "=c"(out[2]), "=d"(out[3])
+                       : "a"(id));
+#else
+  __asm__ __volatile__("cpuid\n"
+                       : "=a"(out[0]), "=b"(out[1]), "=c"(out[2]), "=d"(out[3])
+                       : "a"(id));
+#endif
+}
+
+static void cpuidex(uint32_t out[4], uint32_t id, uint32_t sid) {
+#if defined(_MSC_VER)
+  __cpuidex((int *)out, id, sid);
+#elif defined(__i386__) || defined(_M_IX86)
+  __asm__ __volatile__("movl %%ebx, %1\n"
+                       "cpuid\n"
+                       "xchgl %1, %%ebx\n"
+                       : "=a"(out[0]), "=r"(out[1]), "=c"(out[2]), "=d"(out[3])
+                       : "a"(id), "c"(sid));
+#else
+  __asm__ __volatile__("cpuid\n"
+                       : "=a"(out[0]), "=b"(out[1]), "=c"(out[2]), "=d"(out[3])
+                       : "a"(id), "c"(sid));
+#endif
+}
+
+#endif
+
+enum cpu_feature {
+  SSE2 = 1 << 0,
+  SSSE3 = 1 << 1,
+  SSE41 = 1 << 2,
+  AVX = 1 << 3,
+  AVX2 = 1 << 4,
+  AVX512F = 1 << 5,
+  AVX512VL = 1 << 6,
+  /* ... */
+  UNDEFINED = 1 << 30
+};
+
+#if !defined(BLAKE3_TESTING)
+static /* Allow the variable to be controlled manually for testing */
+#endif
+    ATOMIC_INT g_cpu_features = UNDEFINED;
+
+#if !defined(BLAKE3_TESTING)
+static
+#endif
+    enum cpu_feature
+    get_cpu_features(void) {
+
+  /* If TSAN detects a data race here, try compiling with -DBLAKE3_ATOMICS=1 */
+  enum cpu_feature features = ATOMIC_LOAD(g_cpu_features);
+  if (features != UNDEFINED) {
+    return features;
+  } else {
+#if defined(IS_X86)
+    uint32_t regs[4] = {0};
+    uint32_t *eax = &regs[0], *ebx = &regs[1], *ecx = &regs[2], *edx = &regs[3];
+    (void)edx;
+    features = 0;
+    cpuid(regs, 0);
+    const int max_id = *eax;
+    cpuid(regs, 1);
+#if defined(__amd64__) || defined(_M_X64)
+    features |= SSE2;
+#else
+    if (*edx & (1UL << 26))
+      features |= SSE2;
+#endif
+    if (*ecx & (1UL << 9))
+      features |= SSSE3;
+    if (*ecx & (1UL << 19))
+      features |= SSE41;
+
+    if (*ecx & (1UL << 27)) { // OSXSAVE
+      const uint64_t mask = xgetbv();
+      if ((mask & 6) == 6) { // SSE and AVX states
+        if (*ecx & (1UL << 28))
+          features |= AVX;
+        if (max_id >= 7) {
+          cpuidex(regs, 7, 0);
+          if (*ebx & (1UL << 5))
+            features |= AVX2;
+          if ((mask & 224) == 224) { // Opmask, ZMM_Hi256, Hi16_Zmm
+            if (*ebx & (1UL << 31))
+              features |= AVX512VL;
+            if (*ebx & (1UL << 16))
+              features |= AVX512F;
+          }
+        }
+      }
+    }
+    ATOMIC_STORE(g_cpu_features, features);
+    return features;
+#else
+    /* How to detect NEON? */
+    return 0;
+#endif
+  }
+}
+
+void blake3_compress_in_place(uint32_t cv[8],
+                              const uint8_t block[BLAKE3_BLOCK_LEN],
+                              uint8_t block_len, uint64_t counter,
+                              uint8_t flags) {
+#if defined(IS_X86)
+  const enum cpu_feature features = get_cpu_features();
+  MAYBE_UNUSED(features);
+#if !defined(BLAKE3_NO_AVX512)
+  if (features & AVX512VL) {
+    blake3_compress_in_place_avx512(cv, block, block_len, counter, flags);
+    return;
+  }
+#endif
+#if !defined(BLAKE3_NO_SSE41)
+  if (features & SSE41) {
+    blake3_compress_in_place_sse41(cv, block, block_len, counter, flags);
+    return;
+  }
+#endif
+#if !defined(BLAKE3_NO_SSE2)
+  if (features & SSE2) {
+    blake3_compress_in_place_sse2(cv, block, block_len, counter, flags);
+    return;
+  }
+#endif
+#endif
+  blake3_compress_in_place_portable(cv, block, block_len, counter, flags);
+}
+
+void blake3_compress_xof(const uint32_t cv[8],
+                         const uint8_t block[BLAKE3_BLOCK_LEN],
+                         uint8_t block_len, uint64_t counter, uint8_t flags,
+                         uint8_t out[64]) {
+#if defined(IS_X86)
+  const enum cpu_feature features = get_cpu_features();
+  MAYBE_UNUSED(features);
+#if !defined(BLAKE3_NO_AVX512)
+  if (features & AVX512VL) {
+    blake3_compress_xof_avx512(cv, block, block_len, counter, flags, out);
+    return;
+  }
+#endif
+#if !defined(BLAKE3_NO_SSE41)
+  if (features & SSE41) {
+    blake3_compress_xof_sse41(cv, block, block_len, counter, flags, out);
+    return;
+  }
+#endif
+#if !defined(BLAKE3_NO_SSE2)
+  if (features & SSE2) {
+    blake3_compress_xof_sse2(cv, block, block_len, counter, flags, out);
+    return;
+  }
+#endif
+#endif
+  blake3_compress_xof_portable(cv, block, block_len, counter, flags, out);
+}
+
+
+void blake3_xof_many(const uint32_t cv[8],
+                     const uint8_t block[BLAKE3_BLOCK_LEN],
+                     uint8_t block_len, uint64_t counter, uint8_t flags,
+                     uint8_t out[64], size_t outblocks) {
+  if (outblocks == 0) {
+    // The current assembly implementation always outputs at least 1 block.
+    return;
+  }
+#if defined(IS_X86)
+  const enum cpu_feature features = get_cpu_features();
+  MAYBE_UNUSED(features);
+#if !defined(_WIN32) && !defined(BLAKE3_NO_AVX512)
+  if (features & AVX512VL) {
+    blake3_xof_many_avx512(cv, block, block_len, counter, flags, out, outblocks);
+    return;
+  }
+#endif
+#endif
+  for(size_t i = 0; i < outblocks; ++i) {
+    blake3_compress_xof(cv, block, block_len, counter + i, flags, out + 64*i);
+  }
+}
+
+void blake3_hash_many(const uint8_t *const *inputs, size_t num_inputs,
+                      size_t blocks, const uint32_t key[8], uint64_t counter,
+                      bool increment_counter, uint8_t flags,
+                      uint8_t flags_start, uint8_t flags_end, uint8_t *out) {
+#if defined(IS_X86)
+  const enum cpu_feature features = get_cpu_features();
+  MAYBE_UNUSED(features);
+#if !defined(BLAKE3_NO_AVX512)
+  if ((features & (AVX512F|AVX512VL)) == (AVX512F|AVX512VL)) {
+    blake3_hash_many_avx512(inputs, num_inputs, blocks, key, counter,
+                            increment_counter, flags, flags_start, flags_end,
+                            out);
+    return;
+  }
+#endif
+#if !defined(BLAKE3_NO_AVX2)
+  if (features & AVX2) {
+    blake3_hash_many_avx2(inputs, num_inputs, blocks, key, counter,
+                          increment_counter, flags, flags_start, flags_end,
+                          out);
+    return;
+  }
+#endif
+#if !defined(BLAKE3_NO_SSE41)
+  if (features & SSE41) {
+    blake3_hash_many_sse41(inputs, num_inputs, blocks, key, counter,
+                           increment_counter, flags, flags_start, flags_end,
+                           out);
+    return;
+  }
+#endif
+#if !defined(BLAKE3_NO_SSE2)
+  if (features & SSE2) {
+    blake3_hash_many_sse2(inputs, num_inputs, blocks, key, counter,
+                          increment_counter, flags, flags_start, flags_end,
+                          out);
+    return;
+  }
+#endif
+#endif
+
+#if BLAKE3_USE_NEON == 1
+  blake3_hash_many_neon(inputs, num_inputs, blocks, key, counter,
+                        increment_counter, flags, flags_start, flags_end, out);
+  return;
+#endif
+
+  blake3_hash_many_portable(inputs, num_inputs, blocks, key, counter,
+                            increment_counter, flags, flags_start, flags_end,
+                            out);
+}
+
+// The dynamically detected SIMD degree of the current platform.
+size_t blake3_simd_degree(void) {
+#if defined(IS_X86)
+  const enum cpu_feature features = get_cpu_features();
+  MAYBE_UNUSED(features);
+#if !defined(BLAKE3_NO_AVX512)
+  if ((features & (AVX512F|AVX512VL)) == (AVX512F|AVX512VL)) {
+    return 16;
+  }
+#endif
+#if !defined(BLAKE3_NO_AVX2)
+  if (features & AVX2) {
+    return 8;
+  }
+#endif
+#if !defined(BLAKE3_NO_SSE41)
+  if (features & SSE41) {
+    return 4;
+  }
+#endif
+#if !defined(BLAKE3_NO_SSE2)
+  if (features & SSE2) {
+    return 4;
+  }
+#endif
+#endif
+#if BLAKE3_USE_NEON == 1
+  return 4;
+#endif
+  return 1;
+}

LICENSES/Apache-2.0
@@ -1,0 +1,202 @@
+
+                                 Apache License
+                           Version 2.0, January 2004
+                        http://www.apache.org/licenses/
+
+   TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
+
+   1. Definitions.
+
+      "License" shall mean the terms and conditions for use, reproduction,
+      and distribution as defined by Sections 1 through 9 of this document.
+
+      "Licensor" shall mean the copyright owner or entity authorized by
+      the copyright owner that is granting the License.
+
+      "Legal Entity" shall mean the union of the acting entity and all
+      other entities that control, are controlled by, or are under common
+      control with that entity. For the purposes of this definition,
+      "control" means (i) the power, direct or indirect, to cause the
+      direction or management of such entity, whether by contract or
+      otherwise, or (ii) ownership of fifty percent (50%) or more of the
+      outstanding shares, or (iii) beneficial ownership of such entity.
+
+      "You" (or "Your") shall mean an individual or Legal Entity
+      exercising permissions granted by this License.
+
+      "Source" form shall mean the preferred form for making modifications,
+      including but not limited to software source code, documentation
+      source, and configuration files.
+
+      "Object" form shall mean any form resulting from mechanical
+      transformation or translation of a Source form, including but
+      not limited to compiled object code, generated documentation,
+      and conversions to other media types.
+
+      "Work" shall mean the work of authorship, whether in Source or
+      Object form, made available under the License, as indicated by a
+      copyright notice that is included in or attached to the work
+      (an example is provided in the Appendix below).
+
+      "Derivative Works" shall mean any work, whether in Source or Object
+      form, that is based on (or derived from) the Work and for which the
+      editorial revisions, annotations, elaborations, or other modifications
+      represent, as a whole, an original work of authorship. For the purposes
+      of this License, Derivative Works shall not include works that remain
+      separable from, or merely link (or bind by name) to the interfaces of,
+      the Work and Derivative Works thereof.
+
+      "Contribution" shall mean any work of authorship, including
+      the original version of the Work and any modifications or additions
+      to that Work or Derivative Works thereof, that is intentionally
+      submitted to Licensor for inclusion in the Work by the copyright owner
+      or by an individual or Legal Entity authorized to submit on behalf of
+      the copyright owner. For the purposes of this definition, "submitted"
+      means any form of electronic, verbal, or written communication sent
+      to the Licensor or its representatives, including but not limited to
+      communication on electronic mailing lists, source code control systems,
+      and issue tracking systems that are managed by, or on behalf of, the
+      Licensor for the purpose of discussing and improving the Work, but
+      excluding communication that is conspicuously marked or otherwise
+      designated in writing by the copyright owner as "Not a Contribution."
+
+      "Contributor" shall mean Licensor and any individual or Legal Entity
+      on behalf of whom a Contribution has been received by Licensor and
+      subsequently incorporated within the Work.
+
+   2. Grant of Copyright License. Subject to the terms and conditions of
+      this License, each Contributor hereby grants to You a perpetual,
+      worldwide, non-exclusive, no-charge, royalty-free, irrevocable
+      copyright license to reproduce, prepare Derivative Works of,
+      publicly display, publicly perform, sublicense, and distribute the
+      Work and such Derivative Works in Source or Object form.
+
+   3. Grant of Patent License. Subject to the terms and conditions of
+      this License, each Contributor hereby grants to You a perpetual,
+      worldwide, non-exclusive, no-charge, royalty-free, irrevocable
+      (except as stated in this section) patent license to make, have made,
+      use, offer to sell, sell, import, and otherwise transfer the Work,
+      where such license applies only to those patent claims licensable
+      by such Contributor that are necessarily infringed by their
+      Contribution(s) alone or by combination of their Contribution(s)
+      with the Work to which such Contribution(s) was submitted. If You
+      institute patent litigation against any entity (including a
+      cross-claim or counterclaim in a lawsuit) alleging that the Work
+      or a Contribution incorporated within the Work constitutes direct
+      or contributory patent infringement, then any patent licenses
+      granted to You under this License for that Work shall terminate
+      as of the date such litigation is filed.
+
+   4. Redistribution. You may reproduce and distribute copies of the
+      Work or Derivative Works thereof in any medium, with or without
+      modifications, and in Source or Object form, provided that You
+      meet the following conditions:
+
+      (a) You must give any other recipients of the Work or
+          Derivative Works a copy of this License; and
+
+      (b) You must cause any modified files to carry prominent notices
+          stating that You changed the files; and
+
+      (c) You must retain, in the Source form of any Derivative Works
+          that You distribute, all copyright, patent, trademark, and
+          attribution notices from the Source form of the Work,
+          excluding those notices that do not pertain to any part of
+          the Derivative Works; and
+
+      (d) If the Work includes a "NOTICE" text file as part of its
+          distribution, then any Derivative Works that You distribute must
+          include a readable copy of the attribution notices contained
+          within such NOTICE file, excluding those notices that do not
+          pertain to any part of the Derivative Works, in at least one
+          of the following places: within a NOTICE text file distributed
+          as part of the Derivative Works; within the Source form or
+          documentation, if provided along with the Derivative Works; or,
+          within a display generated by the Derivative Works, if and
+          wherever such third-party notices normally appear. The contents
+          of the NOTICE file are for informational purposes only and
+          do not modify the License. You may add Your own attribution
+          notices within Derivative Works that You distribute, alongside
+          or as an addendum to the NOTICE text from the Work, provided
+          that such additional attribution notices cannot be construed
+          as modifying the License.
+
+      You may add Your own copyright statement to Your modifications and
+      may provide additional or different license terms and conditions
+      for use, reproduction, or distribution of Your modifications, or
+      for any such Derivative Works as a whole, provided Your use,
+      reproduction, and distribution of the Work otherwise complies with
+      the conditions stated in this License.
+
+   5. Submission of Contributions. Unless You explicitly state otherwise,
+      any Contribution intentionally submitted for inclusion in the Work
+      by You to the Licensor shall be under the terms and conditions of
+      this License, without any additional terms or conditions.
+      Notwithstanding the above, nothing herein shall supersede or modify
+      the terms of any separate license agreement you may have executed
+      with Licensor regarding such Contributions.
+
+   6. Trademarks. This License does not grant permission to use the trade
+      names, trademarks, service marks, or product names of the Licensor,
+      except as required for reasonable and customary use in describing the
+      origin of the Work and reproducing the content of the NOTICE file.
+
+   7. Disclaimer of Warranty. Unless required by applicable law or
+      agreed to in writing, Licensor provides the Work (and each
+      Contributor provides its Contributions) on an "AS IS" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+      implied, including, without limitation, any warranties or conditions
+      of TITLE, NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A
+      PARTICULAR PURPOSE. You are solely responsible for determining the
+      appropriateness of using or redistributing the Work and assume any
+      risks associated with Your exercise of permissions under this License.
+
+   8. Limitation of Liability. In no event and under no legal theory,
+      whether in tort (including negligence), contract, or otherwise,
+      unless required by applicable law (such as deliberate and grossly
+      negligent acts) or agreed to in writing, shall any Contributor be
+      liable to You for damages, including any direct, indirect, special,
+      incidental, or consequential damages of any character arising as a
+      result of this License or out of the use or inability to use the
+      Work (including but not limited to damages for loss of goodwill,
+      work stoppage, computer failure or malfunction, or any and all
+      other commercial damages or losses), even if such Contributor
+      has been advised of the possibility of such damages.
+
+   9. Accepting Warranty or Additional Liability. While redistributing
+      the Work or Derivative Works thereof, You may choose to offer,
+      and charge a fee for, acceptance of support, warranty, indemnity,
+      or other liability obligations and/or rights consistent with this
+      License. However, in accepting such obligations, You may act only
+      on Your own behalf and on Your sole responsibility, not on behalf
+      of any other Contributor, and only if You agree to indemnify,
+      defend, and hold each Contributor harmless for any liability
+      incurred by, or claims asserted against, such Contributor by reason
+      of your accepting any such warranty or additional liability.
+
+   END OF TERMS AND CONDITIONS
+
+   APPENDIX: How to apply the Apache License to your work.
+
+      To apply the Apache License to your work, attach the following
+      boilerplate notice, with the fields enclosed by brackets "[]"
+      replaced with your own identifying information. (Don't include
+      the brackets!)  The text should be enclosed in the appropriate
+      comment syntax for the file format. We also recommend that a
+      file or class name and description of purpose be included on the
+      same "printed page" as the copyright notice for easier
+      identification within third-party archives.
+
+   Copyright 2019 Jack O'Connor and Samuel Neves
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.

LICENSES/Makefile.am
@@ -1,4 +1,4 @@
 # SPDX-FileCopyrightText: 2023 Tom Haynes <loghyr@gmail.com>
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-EXTRA_DIST = AGPL-3.0-or-later BSD-3-Clause
+EXTRA_DIST = AGPL-3.0-or-later Apache-2.0 BSD-3-Clause

configure.ac
@@ -904,6 +904,7 @@ AC_CONFIG_FILES([
 	LICENSES/Makefile
 	third_party/Makefile
 	third_party/tomlc99/Makefile
+	third_party/blake3/Makefile
 	lib/Makefile
 	lib/config/Makefile
 	lib/config/tests/Makefile

lib/nfs4/client/Makefile.am
@@ -24,6 +24,7 @@ libreffs_nfs4_client_la_SOURCES = \
 # (the TLS transport implementation).
 libreffs_nfs4_client_la_LIBADD = \
 	$(top_builddir)/lib/tls/libreffs_tls.la \
+	$(top_builddir)/lib/nfs4/common/libreffs_nfs4_common.la \
 	$(OPENSSL_LIBS)
 
 SUBDIRS = tests

lib/nfs4/client/ec_client.h
@@ -600,7 +600,7 @@ struct ec_mirror {
 	bool em_tight_coupled; /* DS supports TRUST_STATEID */
 	/*
 	 * Per-mirror checksum algorithm from the layout
-	 * (ffm_checksum_algorithm).  Used to dispatch CRC computation
+	 * (ffm_checksum_algorithm).  Used to dispatch checksum computation
 	 * on CHUNK_WRITE and verification on CHUNK_READ; the supported-
 	 * set check happens inside mds_layout_get so callers never see
 	 * a layout this client can't compute against.
@@ -746,7 +746,8 @@ int ds_chunk_write(struct mds_session *ds, const uint8_t *fh, uint32_t fh_len,
 		   uint64_t block_offset, uint32_t chunk_size,
 		   const uint8_t *data, uint32_t data_len, uint64_t cohort_id,
 		   uint32_t owner_id, uint32_t layout_client_id,
-		   const stateid4 *stateid, const chunk_guard4 *guard);
+		   uint32_t checksum_algorithm, const stateid4 *stateid,
+		   const chunk_guard4 *guard);
 
 /*
  * ds_chunk_write_repair -- OP_CHUNK_WRITE_REPAIR to a data server.
@@ -759,7 +760,7 @@ int ds_chunk_write_repair(struct mds_session *ds, const uint8_t *fh,
 			  uint32_t chunk_size, const uint8_t *data,
 			  uint32_t data_len, uint64_t cohort_id,
 			  uint32_t owner_id, uint32_t layout_client_id,
-			  const stateid4 *stateid);
+			  uint32_t checksum_algorithm, const stateid4 *stateid);
 
 /*
  * mds_chunk_repaired -- OP_CHUNK_REPAIRED to the MDS.
@@ -821,7 +822,8 @@ struct ec_repair_stats {
  */
 int ds_chunk_read(struct mds_session *ds, const uint8_t *fh, uint32_t fh_len,
 		  uint64_t block_offset, uint32_t count, uint8_t *out_data,
-		  uint32_t chunk_size, uint32_t *nread, const stateid4 *stateid,
+		  uint32_t chunk_size, uint32_t checksum_algorithm,
+		  uint32_t *nread, const stateid4 *stateid,
 		  chunk_owner4 *out_owners, chunk_guard4 *out_guards);
 
 /*

lib/nfs4/client/mds_layout.c
@@ -25,6 +25,7 @@
 
 #include "nfsv42_xdr.h"
 #include "ec_client.h"
+#include "nfs4/chunk_checksum.h"
 
 /* ------------------------------------------------------------------ */
 /* Helpers                                                             */
@@ -64,26 +65,10 @@ static uint32_t parse_owner_id(const char *str, uint32_t len)
  * Enumerate which CHECKSUM_ALG_* values this
  * client knows how to compute end-to-end.
  *
- * Today only CRC32 has a working dispatcher
- * (lib/nfs4/include/nfs4/chunk_checksum.h chunk_checksum_pack_crc32 /
- * chunk_checksum_unpack_crc32).  CRC32C and the hash algorithms are
- * defined in the IANA registry but not yet implemented; a layout that
- * declares any of them must be returned to the MDS with
- * NFS4ERR_LAYOUT_CHECKSUM_NOT_SUPPORTED so the MDS can either reissue
- * with an algorithm the client supports or surface the policy
- * mismatch to the operator.
+ * Keep layout acceptance tied to the common computation dispatcher.
+ * This prevents the client from accepting an algorithm that its CHUNK
+ * I/O path cannot verify.
  */
-static bool layout_checksum_supported(uint32_t alg)
-{
-	switch (alg) {
-	case CHECKSUM_ALG_NONE: /* server has no policy; client computes nothing */
-	case CHECKSUM_ALG_CRC32:
-		return true;
-	default:
-		return false;
-	}
-}
-
 int ec_layout_validate_checksums(const struct ec_layout *layout,
 				 uint32_t *bad_mirror_out)
 {
@@ -92,7 +77,7 @@ int ec_layout_validate_checksums(const struct ec_layout *layout,
 	for (uint32_t i = 0; i < layout->el_nmirrors; i++) {
 		uint32_t alg = layout->el_mirrors[i].em_checksum_algorithm;
 
-		if (!layout_checksum_supported(alg)) {
+		if (!chunk_checksum_supported(alg)) {
 			if (bad_mirror_out)
 				*bad_mirror_out = i;
 			return -ENOTSUP;

lib/nfs4/client/tests/ec_layout_checksum_test.c
@@ -81,10 +81,9 @@ START_TEST(test_validate_all_none)
 }
 END_TEST
 
-START_TEST(test_validate_first_mirror_unsupported)
+START_TEST(test_validate_first_mirror_unknown)
 {
-	struct ec_layout *layout =
-		make_layout(CHECKSUM_ALG_SHA256, CHECKSUM_ALG_CRC32);
+	struct ec_layout *layout = make_layout(99, CHECKSUM_ALG_CRC32);
 
 	ck_assert_ptr_nonnull(layout);
 	uint32_t bad = 0xFF;
@@ -95,10 +94,9 @@ START_TEST(test_validate_first_mirror_unsupported)
 }
 END_TEST
 
-START_TEST(test_validate_second_mirror_unsupported)
+START_TEST(test_validate_second_mirror_unknown)
 {
-	struct ec_layout *layout =
-		make_layout(CHECKSUM_ALG_CRC32, CHECKSUM_ALG_BLAKE3);
+	struct ec_layout *layout = make_layout(CHECKSUM_ALG_CRC32, 99);
 
 	ck_assert_ptr_nonnull(layout);
 	uint32_t bad = 0xFF;
@@ -112,15 +110,14 @@ END_TEST
 START_TEST(test_validate_all_algorithms)
 {
 	/*
-	 * Pin the exact set the client accepts.  When a new
-	 * dispatcher lands (e.g., CRC32C), this test will fail and
-	 * force the supported-set table in mds_layout.c to be
-	 * updated in lock-step.
+	 * Pin the initial checksum registry as the exact set the common
+	 * dispatcher accepts.  Unregistered values stay fail-closed.
 	 */
-	const uint32_t expect_ok[] = { CHECKSUM_ALG_NONE, CHECKSUM_ALG_CRC32 };
-	const uint32_t expect_unsupp[] = {
+	const uint32_t expect_ok[] = {
+		CHECKSUM_ALG_NONE,   CHECKSUM_ALG_CRC32,
 		CHECKSUM_ALG_CRC32C, CHECKSUM_ALG_FLETCHER4,
-		CHECKSUM_ALG_SHA256, CHECKSUM_ALG_SHA512, CHECKSUM_ALG_BLAKE3
+		CHECKSUM_ALG_SHA256, CHECKSUM_ALG_SHA512,
+		CHECKSUM_ALG_BLAKE3
 	};
 
 	for (size_t i = 0; i < sizeof(expect_ok) / sizeof(expect_ok[0]); i++) {
@@ -128,16 +125,6 @@ START_TEST(test_validate_all_algorithms)
 			make_layout(expect_ok[i], expect_ok[i]);
 
 		ck_assert_int_eq(ec_layout_validate_checksums(layout, NULL), 0);
-		free_layout(layout);
-	}
-
-	for (size_t i = 0; i < sizeof(expect_unsupp) / sizeof(expect_unsupp[0]);
-	     i++) {
-		struct ec_layout *layout =
-			make_layout(expect_unsupp[i], CHECKSUM_ALG_CRC32);
-
-		ck_assert_int_eq(ec_layout_validate_checksums(layout, NULL),
-				 -ENOTSUP);
 		free_layout(layout);
 	}
 }
@@ -161,8 +148,8 @@ static Suite *ec_layout_checksum_suite(void)
 
 	tcase_add_test(tc, test_validate_all_crc32);
 	tcase_add_test(tc, test_validate_all_none);
-	tcase_add_test(tc, test_validate_first_mirror_unsupported);
-	tcase_add_test(tc, test_validate_second_mirror_unsupported);
+	tcase_add_test(tc, test_validate_first_mirror_unknown);
+	tcase_add_test(tc, test_validate_second_mirror_unknown);
 	tcase_add_test(tc, test_validate_all_algorithms);
 	tcase_add_test(tc, test_validate_null_layout);
 	suite_add_tcase(s, tc);

commit bf57bf084f9a4afbf81334ea19a7357e2f4c99b7

blake3: build on aarch64 with -Werror
Upstream defines get_cpu_features() unconditionally but calls it only
inside IS_X86 blocks. On aarch64 it is a static function with no
callers, and reffs compiles with -Wall -Wextra -Werror, so
-Wunused-function has broken every arm64 build, native and Docker,
since the copy was vendored in 54d71614417a. The hosted build check
runs on x86_64 and never saw it; the first REFFS integration run did.

Declare the function __attribute__((unused)) and record the local
delta in NOTICE.md so the next refresh of the copy carries it forward.

Fixes: 54d71614417a ("nfs4: implement Flex Files v2 checksums")
Work-Queue-Item: WI-CI-INTEGRATION-REFFS-REPEAT-1
Signed-off-by: Tom Haynes <loghyr@gmail.com>

third_party/blake3/blake3_dispatch.c
@@ -114,6 +114,12 @@ static /* Allow the variable to be controlled manually for testing */
 static
 #endif
     enum cpu_feature
+    get_cpu_features(void) __attribute__((unused));
+
+#if !defined(BLAKE3_TESTING)
+static
+#endif
+    enum cpu_feature
     get_cpu_features(void) {
 
   /* If TSAN detects a data race here, try compiling with -DBLAKE3_ATOMICS=1 */